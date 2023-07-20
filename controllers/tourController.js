40e95a2be9af01d5a675f5fffc53166bc1a303f4
DENGYONGZHEN <YONGZHENDENG111@gmail.com>
2023-07-20 22:07:43 +0900

    before refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,9 +2,72 @@ const Tour = require('../models/tourModel');
 
 // 2) router handlers
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,reatingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //查询过滤的两种方式
+    //1)
+    /* const tours = await Tour.find({ duration: '5', difficulty: 'easy' }); */
+    //2)
+    /* const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy'); */
+    console.log(req.query);
+    //req.query 是一个包含请求参数的对象
+    //1)filter
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((excludedField) => delete queryObj[excludedField]);
+
+    //2) advanced query
+    //{ duration: { $gte: '5' }, difficulty: 'easy' }
+    //{ duration: { gte: '5' }, difficulty: 'easy' }
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`,
+    );
+    let query = Tour.find(JSON.parse(queryString));
+
+    //3) sorting
+    if (req.query.sort) {
+      const sortby = req.query.sort.split(',').join(' ');
+      query = query.sort(sortby);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //4) field limiting
+    if (req.query.fields) {
+      const limitby = req.query.fields.split(',').join(' ');
+      query.select(limitby);
+    } else {
+      query.select('-__v');
+    }
+
+    //5) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numberTour = await Tour.countDocuments();
+      if (skip >= numberTour) {
+        throw new Error('This page does not exit');
+      }
+    }
+
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 //只用于这个路由的中间件可以放在这个路由中
 //router.param('id', tourController.checkID);
+//get(中间件, 控制器)
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/').get(tourController.getAllTours).post(tourController.postTour);
 router

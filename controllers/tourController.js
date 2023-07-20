442e4bb268fd5c6968f5ae243edd7dc9593db094
DENGYONGZHEN <YONGZHENDENG111@gmail.com>
2023-07-20 23:20:02 +0900

    refactored

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 // 2) router handlers
 
@@ -11,63 +12,14 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    //查询过滤的两种方式
-    //1)
-    /* const tours = await Tour.find({ duration: '5', difficulty: 'easy' }); */
-    //2)
-    /* const tours = await Tour.find()
-      .where('duration')
-      .equals(5)
-      .where('difficulty')
-      .equals('easy'); */
-    console.log(req.query);
-    //req.query 是一个包含请求参数的对象
-    //1)filter
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((excludedField) => delete queryObj[excludedField]);
+    const feature = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitField()
+      .pagination();
 
-    //2) advanced query
-    //{ duration: { $gte: '5' }, difficulty: 'easy' }
-    //{ duration: { gte: '5' }, difficulty: 'easy' }
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`,
-    );
-    let query = Tour.find(JSON.parse(queryString));
+    const tours = await feature.query;
 
-    //3) sorting
-    if (req.query.sort) {
-      const sortby = req.query.sort.split(',').join(' ');
-      query = query.sort(sortby);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //4) field limiting
-    if (req.query.fields) {
-      const limitby = req.query.fields.split(',').join(' ');
-      query.select(limitby);
-    } else {
-      query.select('-__v');
-    }
-
-    //5) pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numberTour = await Tour.countDocuments();
-      if (skip >= numberTour) {
-        throw new Error('This page does not exit');
-      }
-    }
-
-    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,

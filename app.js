f391d891914bc0a8b8526ff8f27666ebc5393c55
DENGYONGZHEN <YONGZHENDENG111@gmail.com>
2023-07-25 21:37:13 +0900

    hpp

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
@@ -7,16 +13,50 @@ const userRouter = require('./routes/userRouter');
 
 const app = express();
 
-// 1) middleware
+// 1) Goobal middleware
 
+//set security HTTP headers
+app.use(helmet());
+
+//development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-//express.json()  middleware
-app.use(express.json());
+//声明 限制同一ip频繁访问
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try in an hour!',
+});
+//调用
+app.use('/api', limiter);
+
+//express.json()  middleware   Body parser  reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
 //static files
 app.use(express.static(`${__dirname}/public`));
+
 //自定义 middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
